c992e171df06d25829710c497f9caa080576c323
박찬진 <chanjinbag57@gmail.com>
2023-04-06 06:21:35 +0000

    계산기

---
+++ calc/calc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+float add() {
+	float sum =0, num1=0, num2=0;
+printf("첫번째 숫자 입력 : ");
+scanf("%f", &num1);
+printf("두번째 숫자 입력 : ");
+scanf("%f", &num2);
+sum = num1 + num2;
+return sum ;
+}
+
+float sub() {
+	int result =0, num1=0, num2=0;
+	
+	return result;
+}
+	
+	int main() {
+	printf("덧셈결과 : %f\n", add());
+}
